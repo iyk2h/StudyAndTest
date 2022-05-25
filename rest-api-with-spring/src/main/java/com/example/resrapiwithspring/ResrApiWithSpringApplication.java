17f9b1adcae314cd8d33eaa162c9ca4538424b54
youngkyunIm <yee0230@naver.com>
2022-05-25 15:44:41 +0900

    project creation

---
+++ rest-api-with-spring/src/main/java/com/example/resrapiwithspring/ResrApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.resrapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResrApiWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResrApiWithSpringApplication.class, args);
+	}
+
+}
