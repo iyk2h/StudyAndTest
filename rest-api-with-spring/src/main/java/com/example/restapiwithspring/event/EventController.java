217e0c82ab608fc601214daeda9d0d38b68bc0c9
youngkyunIm <yee0230@naver.com>
2022-05-25 22:03:24 +0900

    DB에 자동 생성된 값으로 나오는지 테스트

--- rest-api-with-spring/src/main/java/com/example/restapiwithspring/event/EventController.java
+++ rest-api-with-spring/src/main/java/com/example/restapiwithspring/event/EventController.java
@@ -15,10 +15,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE )
 public class EventController {
 
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
     @PostMapping
     public ResponseEntity creatEvent(@RequestBody @NotNull Event event) {
-        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
-        event.setId(10);
+        Event newEvent = this.eventRepository.save(event);
+        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createUri).body(event);
     }
 }
---
+++ rest-api-with-spring/src/main/java/com/example/restapiwithspring/event/EventRepository.java
@@ -0,0 +1,6 @@
+package com.example.restapiwithspring.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
