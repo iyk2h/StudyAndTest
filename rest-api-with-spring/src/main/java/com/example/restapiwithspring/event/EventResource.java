27a5ac1b3464d3ff924574d49b2c7a125ee8f699
youngkyunIm <yee0230@naver.com>
2022-05-26 23:23:31 +0900

    Add Spring REST Docs

--- rest-api-with-spring/src/main/java/com/example/restapiwithspring/event/EventResource.java
+++ rest-api-with-spring/src/main/java/com/example/restapiwithspring/event/EventResource.java
@@ -1,36 +1,39 @@
 package com.example.restapiwithspring.event;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
-
-import java.util.Arrays;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 //    방법 1
-//public class EventResource extends RepresentationModel {
-//
-//    @JsonUnwrapped
-//    private Event event;
-//
+public class EventResource extends RepresentationModel {
+
+    @JsonUnwrapped
+    private Event event;
+
 //    public EventResource(Event event) {
 //        this.event = event;
 //    }
-//
-//    public Event getEvent() {
-//        return event;
-//    }
-//}
 
-// 방법 2
+    public EventResource(Event event) {
+        this.event = event;
+        this.add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+        this.add(linkTo(EventController.class).withRel("query-events"));
+    }
 
-public class EventResource extends EntityModel<Event> {
-    public EventResource(Event event, Link... links) {
-        super(event, Arrays.asList(links));
-        Link link = linkTo(EventController.class).slash(event.getId()).withSelfRel();
-        add(link);
-        add(linkTo(EventController.class).withRel("query-events"));
+    public Event getEvent() {
+        return event;
     }
 }
+
+// 방법 2
+//
+//public class EventResource extends EntityModel<Event> {
+//    public EventResource(Event event, Link... links) {
+//        super(event, Arrays.asList(links));
+//        Link link = linkTo(EventController.class).slash(event.getId()).withSelfRel();
+//        add(link);
+//        add(linkTo(EventController.class).withRel("query-events"));
+//    }
+//}
