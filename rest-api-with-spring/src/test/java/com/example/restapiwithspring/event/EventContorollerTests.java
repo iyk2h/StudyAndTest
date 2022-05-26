c346429d681c062b2186f87668a2a005f7995b56
youngkyunIm <yee0230@naver.com>
2022-05-26 22:41:29 +0900

    Hateos 적용

--- rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventContorollerTests.java
+++ rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventContorollerTests.java
@@ -65,6 +65,9 @@ public class EventContorollerTests {
                 .andExpect(jsonPath("free").value(false))
                 .andExpect(jsonPath("offline").value(true))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-event").exists())
         ;
     }
 
--- rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventTest.java
+++ rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventTest.java
@@ -1,6 +1,13 @@
 package com.example.restapiwithspring.event;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runners.Parameterized;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,61 +38,44 @@ class EventTest {
 
     }
 
-    @Test
-    public void testFree() {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false"
+    })
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         //Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
         // When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
-
-        //Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-
-        //Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
-    public void testOffline() {
+    @ParameterizedTest
+    @MethodSource("isOffline")
+    public void testOffline(String location, boolean isOffline) {
         //Given
         Event event = Event.builder()
-                .location("강남역 네이버 ")
+                .location(location)
                 .build();
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
 
-        event = Event.builder()
-                .build();
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isOffline()).isFalse();
+    private static Stream<Arguments> isOffline() {
+        return Stream.of(
+                Arguments.of("강남역", true),
+                Arguments.of(null, false),
+                Arguments.of("", false)
+        );
     }
 }
