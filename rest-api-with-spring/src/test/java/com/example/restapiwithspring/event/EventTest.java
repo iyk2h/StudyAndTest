0924d25caaafcca94b1ffabcdd3bdf0b6d166f14
youngkyunIm <yee0230@naver.com>
2022-05-26 20:54:26 +0900

    하드한 테스트 코드

--- rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventTest.java
+++ rest-api-with-spring/src/test/java/com/example/restapiwithspring/event/EventTest.java
@@ -30,4 +30,62 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
 
     }
+
+    @Test
+    public void testFree() {
+        //Given
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isTrue();
+
+        //Given
+        event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isFalse();
+
+        //Given
+        event = Event.builder()
+                .basePrice(0)
+                .maxPrice(100)
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isFalse();
+
+    }
+
+    @Test
+    public void testOffline() {
+        //Given
+        Event event = Event.builder()
+                .location("강남역 네이버 ")
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isOffline()).isTrue();
+
+        event = Event.builder()
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isOffline()).isFalse();
+    }
 }
